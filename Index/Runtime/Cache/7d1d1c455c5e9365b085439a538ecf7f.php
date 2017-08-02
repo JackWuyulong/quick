131437fae34acf8acf3f109b0d8b4107ceac7329
1245711844@qq.com <1245711844@qq.com>
2017-08-02 10:56:43 +0800

    基本结构

---
+++ Index/Runtime/Cache/7d1d1c455c5e9365b085439a538ecf7f.php
@@ -0,0 +1,29 @@
+<?php if (!defined('THINK_PATH')) exit();?><!doctype html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Document</title>
+	<link rel="stylesheet" href="__ROOT__/Index/Common/css/index.css" />
+</head>
+<body>
+	<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Document</title>
+	<link rel="stylesheet" href="__ROOT__/Index/Common/css/index.css" />
+	
+</head>
+<body>
+	<div class="bottom">
+		<div class="advantage">
+			<img src="" alt="" /><span>免押金</span>
+			<img src="" alt="" /><span>低租金</span>
+			<img src="" alt="" /><span>随时还</span>
+			<img src="" alt="" /><span>全程保</span>
+		</div>
+	</div>
+</body>
+</html>
+</body>
+</html>
